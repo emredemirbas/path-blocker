cb626b9ced3201b5b7b2c3702fedd1ad9b300f9a
Emre Demirbaş <emredemirbas09@gmail.com>
2024-10-14 23:51:13 +0300

    Update
    Some utility functions are implemented.

--- src/Level.java
+++ src/Level.java
@@ -1,13 +1,13 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 public class Level {
     private boolean[][] world;
 
     private List<int[]> agentPositions;
+
     private Agent agent;
 
     private int width;
@@ -18,7 +18,7 @@ public class Level {
 
     private int[] startPosition;
 
-    public Level(int width, int height, int[] goalPosition, int[] startPosition, int[][] wallPositions, Agent agent){
+    public Level(int width, int height, int[] goalPosition, int[] startPosition, int[][] wallPositions, Agent agent) {
         this.world = new boolean[width][height];
         this.agent = agent;
         this.width = width;
@@ -27,29 +27,30 @@ public class Level {
         this.startPosition = startPosition;
         this.agentPositions.add(startPosition);
 
-        for(int[] wallPosition:wallPositions){
+        for (int[] wallPosition : wallPositions) {
             this.world[wallPosition[0]][wallPosition[1]] = true;
         }
 
     }
 
-    public Level(String path){
+    public Level(String path) {
+        this.agentPositions = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                if(line.startsWith("s")){
+                if (line.startsWith("s")) {
                     String[] splittedLine = line.substring(1).trim().split(",");
                     this.startPosition = new int[]{Integer.parseInt(splittedLine[0]), Integer.parseInt(splittedLine[1])};
                     this.agentPositions.add(startPosition);
                 } else if (line.startsWith("g")) {
                     String[] splittedLine = line.substring(1).trim().split(",");
                     this.goalPosition = new int[]{Integer.parseInt(splittedLine[0]), Integer.parseInt(splittedLine[1])};
-                } else if(line.startsWith("w")){
+                } else if (line.startsWith("w")) {
                     this.width = Integer.parseInt(line.substring(1));
-                }else if(line.startsWith("h")){
+                } else if (line.startsWith("h")) {
                     this.height = Integer.parseInt(line.substring(1));
                     this.world = new boolean[this.width][this.height];
-                }else{
+                } else {
                     String[] splittedLine = line.trim().split(",");
                     int[] wallPosition = {Integer.parseInt(splittedLine[0]), Integer.parseInt(splittedLine[1])};
                     this.world[wallPosition[0]][wallPosition[1]] = true;
@@ -60,52 +61,116 @@ public class Level {
         }
     }
 
-    private void putWall(int x, int y){
-        if(x<this.width && y<this.height && x>=0 && y>=0){
+    private void putWall(int x, int y) {
+        if (x < this.width && y < this.height && x >= 0 && y >= 0) {
             this.world[x][y] = true;
         }
+    }
+
+    private void putWall(Direction direction) {
+        int x = this.getAgentX();
+        int y = this.getAgentY();
+        switch (direction) {
+            case LEFT -> x = this.getAgentLeft();
+            case RIGHT -> x = this.getAgentRight();
+            case UP -> y = this.getAgentUp();
+            case DOWN -> y = this.getAgentDown();
+        }
+        this.putWall(x, y);
     }
 
 //    private boolean isMovable(int x, int y){
 //
 //    }
 
-    private boolean isCoordinateInsideWorld(int x, int y){
-        return x>=0 && x<this.width && y>=0 && y<this.height;
+    private int getAgentX() {
+        return this.agentPositions.get(this.agentPositions.size() - 1)[0];
     }
 
-    private boolean isWallAtCoordinate(int x, int y){
+    private int getAgentY() {
+        return this.agentPositions.get(this.agentPositions.size() - 1)[1];
+    }
+
+    private int[] getAgentXY() {
+        return new int[]{getAgentX(), getAgentY()};
+    }
+
+    private int[] nextAgentXY(Direction direction) {
+        int x = this.getAgentX();
+        int y = this.getAgentY();
+        if (direction == Direction.LEFT) {
+            x = this.getAgentLeft();
+        } else if (direction == Direction.RIGHT) {
+            x = this.getAgentRight();
+        } else if (direction == Direction.UP) {
+            y = this.getAgentUp();
+        } else if (direction == Direction.DOWN) {
+            y = this.getAgentDown();
+        }
+        return new int[]{x, y};
+    }
+
+    private int getAgentLeft() {
+        return this.getAgentX() - 1;
+    }
+
+    private int getAgentRight() {
+        return this.getAgentX() + 1;
+    }
+
+    private int getAgentUp() {
+        return this.getAgentY() + 1;
+    }
+
+    private int getAgentDown() {
+        return this.getAgentY() - 1;
+    }
+
+    private boolean isMovable(Direction direction) {
+        int currentX = getAgentX();
+        int currentY = getAgentY();
+        switch (direction) {
+            case LEFT:
+                return this.isCoordinateInsideWorld(currentX - 1, currentY) && !this.isWallAtCoordinate(currentX - 1, currentY);
+            case RIGHT:
+                return this.isCoordinateInsideWorld(currentX + 1, currentY) && !this.isWallAtCoordinate(currentX + 1, currentY);
+            case UP:
+                return this.isCoordinateInsideWorld(currentX, currentY + 1) && !this.isWallAtCoordinate(currentX, currentY + 1);
+            case DOWN:
+                return this.isCoordinateInsideWorld(currentX, currentY - 1) && !this.isWallAtCoordinate(currentX, currentY - 1);
+        }
+        return false;
+    }
+
+    private boolean isCoordinateInsideWorld(int x, int y) {
+        return x >= 0 && x < this.width && y >= 0 && y < this.height;
+    }
+
+    private boolean isWallAtCoordinate(int x, int y) {
         return this.world[x][y];
     }
-    public List<Directions> getMovableDirections(){
-        List<Directions> movableDirections = new ArrayList<>();
-        int[] currentAgentPosition = this.agentPositions.get(agentPositions.size()-1);
-        int agentX = currentAgentPosition[0];
-        int agentY = currentAgentPosition[1];
-        int agentLeft = currentAgentPosition[0] - 1;
-        int agentRight = currentAgentPosition[0] + 1;
-        int agentUp = currentAgentPosition[1] + 1;
-        int agentDown = currentAgentPosition[1] - 1;
 
-        //Check left
-        if(isCoordinateInsideWorld(agentLeft, agentY) && !isWallAtCoordinate(agentLeft, agentY)){
-            movableDirections.add(Directions.LEFT);
-        }
-        //Check right
-        if(isCoordinateInsideWorld(agentRight, agentY) && !isWallAtCoordinate(agentRight, agentY)){
-            movableDirections.add(Directions.RIGHT);
-        }
+    public List<Direction> getMovableDirections() {
+        List<Direction> movableDirections = new ArrayList<>();
 
-        if(isCoordinateInsideWorld(agentX, agentUp) && !isWallAtCoordinate(agentX, agentUp)){
-            movableDirections.add(Directions.UP);
-        }
+        Direction[] directions = {Direction.LEFT, Direction.RIGHT, Direction.DOWN, Direction.UP};
 
-        if(isCoordinateInsideWorld(agentX, agentDown) && !isWallAtCoordinate(agentX, agentDown)){
-            movableDirections.add(Directions.DOWN);
+        for (Direction d : directions) {
+            if (this.isMovable(d)) {
+                movableDirections.add(d);
+            }
         }
 
         return movableDirections;
+    }
 
+    public void moveAgent(Direction direction) {
+        int x = this.getAgentX();
+        int y = this.getAgentY();
+        while (this.isMovable(direction)) {
+            this.putWall(direction);
+            this.agentPositions.add(this.nextAgentXY(direction));
+        }
     }
 
     public int getWidth() {
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Level level = new Level("levels/level01/level01.txt");
+        level.moveAgent(Direction.RIGHT);
         LevelImageGenerator.createAndSaveLevelImage("deneme.png", level);
     }
 }
